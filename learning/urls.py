7159fc756b87c265f6209ad1316afa0fde4ff47d
Sisojack <jackjrsiso@gmail.com>
2024-07-10 10:53:16 +0300

    cont commits

--- learning/urls.py
+++ learning/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name= 'home')
+    path('', views.index, name= 'home'),
+    path('404/', views.notfound, name= '404'),
+    path('blog-single/', views.blog, name= 'blog-single'),
+    path('contact/', views.contact, name= 'contact'),
+    path('portfolio/', views.portfolio, name= 'portfolio-details'),
+    path('#/', views.ipnav)
+
 ]
--- learning/views.py
+++ learning/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def notfound(request):
+    return render(request, '404.html')
+def blog(request):
+    return render(request, 'blog-single.html')
+def contact(request):
+    return render(request, 'contact.html')
+def portfolio(request):
+    return render(request, 'portfolio-details.html')
+def ipnav(request):
+    return render(request, '#')
