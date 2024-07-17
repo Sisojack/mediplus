6fd61cca0cec6b829bc09ff4053f39cbcb5ee70f
Sisojack <jackjrsiso@gmail.com>
2024-07-17 17:14:21 +0300

    fol commit

--- learning/models.py
+++ learning/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Appointment(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    doctor = models.CharField(max_length=100)
+    date = models.DateField()
+    message = models.TextField()
+    def __str__(self):
+        return self.name
--- learning/views.py
+++ learning/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Appointment
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,15 @@ def portfolio(request):
     return render(request, 'portfolio-details.html')
 def ipnav(request):
     return render(request, '#')
+def appointment(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        department = request.POST.get('department')
+        doctor = request.POST.get('doctor')
+        date = request.POST.get('date')
+        message = request.POST.get('message')
+
+        saveappointment = Appointment(name=name, email=email, phone=phone, department=department, doctor=doctor, date=date, message=message)
+        saveappointment.save()
